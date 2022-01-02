961fa1e6f2c7bab264fa9cfe0a4aaddeb660f5c3
NerVod <benjamin.jeannerot@gmail.com>
2022-01-02 19:37:53 +0100

    ex 10

--- exercices2/exercice10.js
+++ exercices2/exercice10.js
@@ -118,7 +118,170 @@
     https://nodejs.org/api/url.html#class-urlsearchparams
   Elle permet, à partir d'une URL, de créer un objet contenant les paramètres de query string.
 **/
+const http = require("http");
+const fs = require("fs");
+const server = http.createServer();
+const path = require("path");
+const process = require("process");
+const { constants } = require("buffer");
+const mime = require("mime");
+const { brotliCompressSync } = require("zlib");
 
+
+
+let dossierExecution = process.cwd().normalize();
+let filePath = "";
+let status;
+
+let dateRequete = new Date();
+
+
+
+///////////////////////////////////////////////////////////////
+///// verif nom dossier des fichiers
+const lesDossiers = function () {
+  console.log("nom du fichier source : ", __filename);
+  console.log("nom du dossier source :", __dirname);
+  let dossierExecution = process.cwd().normalize();
+  console.log(dossierExecution);
+  return dossierExecution
+};
+lesDossiers();
+//////////////////////////////////////////////////////////////
+
+
+server.on("request", function (req, res) {
+  const method = req.method;
+  console.log(`la méthode est en : `, method);
+  const urlEnFormatBrut = req.url;
+  const parsedUrl = new URL(urlEnFormatBrut, `http://${req.rawHeaders[1]}`);
+
+  parsedUrl;
+  console.log(parsedUrl);
+
+  let suffixe = parsedUrl.pathname;
+
+  let file = dossierExecution + suffixe;
+  let file404 = dossierExecution + '\\404.html'
+  
+
+  
+
+  fs.access(file, constants.F_OK | constants.W_OK, (err) => {
+    if (err) {
+      status = 404;
+      filePath = file404;
+      res.writeHead(status, {
+        "Content-Type": "text/html; charset=utf8",
+      });
+      fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) throw err;
+        res.write(data);
+        res.end();
+      });
+    } else {
+      status = 200;
+      filePath = file;
+      let mimeType = mime.getType(path.extname(filePath));
+      // console.log(`filepath : `, filePath)
+      // console.log(`mime type : `,mimeType);
+      res.writeHead(status, {
+        "Content-Type": mimeType,
+        
+      });
+      fs.readFile(filePath, "utf8", (err, data) => {
+        if (err) throw err;
+
+
+        const regexDate = /##dateDuJour##/;
+
+
+        
+        const regexNom= /{{ nom }}/;
+        let urlLastName = parsedUrl.searchParams.get('name');
+        console.log(`nom de l'URL : `, urlLastName)
+
+        const regexPrenom = /{{ prenom }}/;
+        let urlFirstName = parsedUrl.searchParams.get('firstname');
+        console.log(`prénom de l'URL : `, urlFirstName)
+
+        if(urlLastName === null){
+          urlLastName = "Nom";
+        }
+        if(urlFirstName === null){
+          urlFirstName = "Prénom"
+        }
+
+        ///////// formulaire //////////////////
+        const regexTitre = /{{ titre }}/;
+        
+        const regexDescription = /{{ description }}/;
+        const regexDateFormulaire = /{{ date }}/;
+        
+        if(method === "get") {
+
+          let urlTitre = parsedUrl.searchParams.get('titre');
+          
+          let urlDescriptif= parsedUrl.searchParams.get('descriptif')
+          
+          let urlDate = parsedUrl.searchParams.get('date');
+
+          data = data.replace(regexTitre, urlTitre);
+          data = data.replace(regexDescription, urlDescriptif);
+          data = data.replace(regexDateFormulaire, urlDate);
+          
+          
+          data = data.replace(regexNom, urlLastName);
+          data = data.replace(regexPrenom, urlFirstName);
+          
+          data = data.replace(regexDate, dateRequete);
+          
+          data = data.toString();
+          return data;
+        }
+
+        let donnees = "";
+        if( method === "post"){
+
+          req.on("data", (morceauDeDonnee) => {
+            donnees += morceauDeDonnee;
+            donnees = donnees.toString();
+            return donnees
+          })
+          req.on('end', function(){
+            donnees;
+            console.log(`les données : `, donnees)
+            return donnees;
+          })
+
+          let queryStringMethodPost = new URLSearchParams(donnees);
+          console.log(`la query string de post :` , queryStringMethodPost)
+
+          let urlTitrePost = queryStringMethodPost.get('titre');
+          let urlDescriptifPost = queryStringMethodPost.get('descriptif');
+          let urlDatePost = queryStringMethodPost.get('date')
+
+          data = data.replace(regexTitre, urlTitrePost);
+          data = data.replace(regexDescription, urlDescriptifPost);
+          data = data.replace(regexDateFormulaire, urlDatePost);
+
+          data = data.toString();
+          
+          return data;
+
+        }
+          
+
+        res.write(data);
+        res.end();
+      });
+    }
+  });
+});
+
+// sur fichier about.html : query string : ?name=Jeannerot&firstname=Benjamin
+
+server.listen(8080);
 /**
  * Sami Radi - VirtuoWorks® - tous droits réservés©
 **/
